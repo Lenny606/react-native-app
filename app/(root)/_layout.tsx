aa8b2a0f09e30643a04eb4070eca7b776fab9bab
--help <you@example.com>
2024-12-24 15:14:14 +0100

    auth logic

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,25 @@
+import {useGlobalContext} from "@/lib/global-provider";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {ActivityIndicator} from "react-native";
+import {Navigator, Redirect, Slot} from "expo-router";
+
+export default function AppLayout() {
+    const {isLoggedIn, loading, refetch} = useGlobalContext()
+
+    if (loading) {
+        return (
+            <SafeAreaView className={'bg-white h-full flex justify-center items-center'}>
+                {/*LOADER*/}
+                <ActivityIndicator className={'text-primary-300'} size={'large'}/>
+            </SafeAreaView>
+        )
+    }
+    if (!isLoggedIn) {
+        return (
+            <Redirect href={'/sign-in'}/>
+        )
+    }
+
+    // Slot is a placeholder for the current screen
+    return Slot
+}
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,30 @@
-import {View, Text, ScrollView, Image, TouchableOpacity} from 'react-native';
+import {View, Text, ScrollView, Image, TouchableOpacity, Alert} from 'react-native';
 import {SafeAreaView} from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import {login} from "@/lib/appwrite";
+import {useGlobalContext} from "@/lib/global-provider";
+import {Redirect} from "expo-router";
 
 const SignIn = () => {
-    const handleLogin = () => {
 
+    const {isLoggedIn, loading, refetch} = useGlobalContext()
+
+    if (isLoggedIn && !loading) {
+        return <Redirect href={'/'} />
+    }
+
+    const handleLogin = async () => {
+
+        const result = await login()
+
+        if (result) {
+            console.log('Login success')
+            refetch()
+        } else {
+            console.log('Login fail')
+            Alert.alert('Failed to login')
+        }
     }
     return (
         <SafeAreaView className={'h-full bg-white'}>
