5f38af3a6565503a47a294f46d1b9486dd5afa51
--help <you@example.com>
2024-12-25 14:15:27 +0100

    homepage header

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,25 @@
-import {Text, View} from "react-native";
+import {Image, Text, View} from "react-native";
 import {Link} from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 export default function Index() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text className={"text-3xl font-bold my-10 font-rubik"}>Home</Text>
-            {/*<Link href={"/sign-in"}>Sign In</Link>*/}
-            {/*<Link href={"/explore"}>Explore</Link>*/}
-            {/*<Link href={"/profile"}>Profile</Link>*/}
-            {/*<Link href={"/properties/1"}>Property</Link>*/}
-        </View>
+       <SafeAreaView className={'h-full bg-white'}>
+           <View className={'px-5'}>
+               <View className={'flex flex-row items-center justify-between mt-5'}>
+                   <View className={'flex flex-row items-center'}>
+                       <Image source={images.avatar} className={'rounded-full size-10'}/>
+                       <View className={'flex-col flex items-start justify-center ml-2'}>
+                           <Text className={'text-xs font-rubik text-black-100'}>Hello</Text>
+                           <Text className={'text-base font-rubikMedium text-black-300'}>Tom</Text>
+                       </View>
+                   </View>
+                   <Image source={icons.bell} className={'size-6'} />
+               </View>
+           </View>
+
+       </SafeAreaView>
     );
 }
